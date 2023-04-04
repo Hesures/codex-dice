96e1a4c5e6e9efa3a71d80f69659fa8ad1a0613f
Hesures <128929471+Hesures@users.noreply.github.com>
2023-04-04 23:19:07 +0200

    Rolling dice

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+
+// ROLLING DICES BY CLICKING
+document.querySelector(".dice-button").addEventListener("click",function(){
+// DICE 1
+let rollDice1 = Math.floor(Math.random()*6+1);
+let sourceDiceImg1 = "images/dice" + rollDice1 + ".png";
+let changeDiceImg1 = document.querySelector(".img-dice1").setAttribute("src",sourceDiceImg1);
+// DICE 2
+let rollDice2 = Math.floor(Math.random()*6+1);
+let sourceDiceImg2 = "images/dice" + rollDice2 + ".png";
+let changeDiceImg2 = document.querySelector(".img-dice2").setAttribute("src",sourceDiceImg2);
+});
+
+// let rollResult = rollDice1 + rollDice2;
+
+
