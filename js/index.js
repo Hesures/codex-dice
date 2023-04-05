45a8bd66b3424b92eb3644cf83780dbcfbd4a2fe
Hesures <128929471+Hesures@users.noreply.github.com>
2023-04-05 19:52:07 +0200

    Updated 05/04/23
    Designed added. Responsive added. Qualities added.

--- js/index.js
+++ js/index.js
@@ -2,19 +2,27 @@
 
 // ROLLING DICES BY CLICKING
 document.querySelector(".dice-button").addEventListener("click",function(){
-// ROLLING AUDIO
-let diceRoll = new Audio("sound/rolldice.mp3");
+// rolling audio
+var diceRoll = new Audio("sound/rolldice.mp3");
 diceRoll.play();
-// DICE 1
-let rollDice1 = Math.floor(Math.random()*6+1);
-let sourceDiceImg1 = "images/dice" + rollDice1 + ".png";
-let changeDiceImg1 = document.querySelector(".img-dice1").setAttribute("src",sourceDiceImg1);
-// DICE 2
-let rollDice2 = Math.floor(Math.random()*6+1);
-let sourceDiceImg2 = "images/dice" + rollDice2 + ".png";
-let changeDiceImg2 = document.querySelector(".img-dice2").setAttribute("src",sourceDiceImg2);
+// dice 1
+var rollDice1 = Math.floor(Math.random()*6+1);
+var sourceDiceImg1 = "images/dice" + rollDice1 + ".png";
+var changeDiceImg1 = document.querySelector(".img-dice1").setAttribute("src",sourceDiceImg1);
+// dice 2
+var rollDice2 = Math.floor(Math.random()*6+1);
+var sourceDiceImg2 = "images/dice" + rollDice2 + ".png";
+var changeDiceImg2 = document.querySelector(".img-dice2").setAttribute("src",sourceDiceImg2);
+
+// result
+var rollResult = rollDice1 + rollDice2;
+var changeRollResult = document.querySelector(".diceRollResult").innerHTML=rollResult;
+document.querySelector(".diceRollResult").style.fontSize="2.5rem";
+document.querySelector(".diceRollResult").style.fontFamily="'Cinzel'";
+
 });
 
-// let rollResult = rollDice1 + rollDice2;
+
+
 
 
