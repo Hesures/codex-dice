6a594674db9efdb030e627866d1295ed4717ebbf
Hesures <128929471+Hesures@users.noreply.github.com>
2023-04-28 13:52:15 +0200

    Refactorize variables in json

--- js/index.js
+++ js/index.js
@@ -1,26 +1,33 @@
-
+const diceButton = document.querySelector(".dice-button");
 
 // ROLLING DICES BY CLICKING
-document.querySelector(".dice-button").addEventListener("click",function(){
-// rolling audio
-var diceRoll = new Audio("sound/rolldice.mp3");
-diceRoll.play();
-// dice 1
-var rollDice1 = Math.floor(Math.random()*6+1);
-var sourceDiceImg1 = "images/dice" + rollDice1 + ".png";
-var changeDiceImg1 = document.querySelector(".img-dice1").setAttribute("src",sourceDiceImg1);
-// dice 2
-var rollDice2 = Math.floor(Math.random()*6+1);
-var sourceDiceImg2 = "images/dice" + rollDice2 + ".png";
-var changeDiceImg2 = document.querySelector(".img-dice2").setAttribute("src",sourceDiceImg2);
+diceButton.addEventListener("click",function(){
 
-// result
-var rollResult = rollDice1 + rollDice2;
-var changeRollResult = document.querySelector(".diceRollResult").innerHTML=rollResult;
-document.querySelector(".diceRollResult").style.fontSize="2.5rem";
-document.querySelector(".diceRollResult").style.fontFamily="'Cinzel'";
+    // rolling audio
+    var diceAudio = new Audio("sound/rolldice.mp3");
+    diceAudio.play();
 
+    // dice 1
+    var diceRoll1 = Math.floor(Math.random()*6+1);
+    var sourceDiceImg1 = "images/dice" + diceRoll1 + ".png";
+    var changeDiceImg1 = document.querySelector(".img-dice1");
+    changeDiceImg1.setAttribute("src",sourceDiceImg1);
+
+    // dice 2
+    var diceRoll2 = Math.floor(Math.random()*6+1);
+    var sourceDiceImg2 = "images/dice" + diceRoll2 + ".png";
+    var changeDiceImg2 = document.querySelector(".img-dice2");
+    changeDiceImg2.setAttribute("src",sourceDiceImg2);
+
+    // result
+    var rollResult = diceRoll1 + diceRoll2;
+    var changeRollResult = document.querySelector(".diceRollResult")
+    changeRollResult.innerHTML=rollResult;
+    document.querySelector(".diceRollResult").style.fontSize="2.5rem";
+    document.querySelector(".diceRollResult").style.fontFamily="'Cinzel'";
+    
 });
+
 
 
 
